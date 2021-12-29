c19d411f4a3bb0c5d0e1b4fdd1aa8cf8eb813c68
Deepika S R <deepikasanathanan1234@gmail.com>
2021-12-29 23:57:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// document.getElementById("count-el").innerText = 10
+
+let countEl = document.getElementById("count-el")
+let count = 0
+function increment() {
+    count += 1
+    countEl.innerText = count
+    console.log(count)
+}
+function decrement(){
+    count -= 1
+    countEl.innerText = count
+    console.log(count)
+    
+}
+
+     alert("Hey! \n Glad you are here");
